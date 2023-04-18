8d1b6a00efe99635e09b8b5694dffbaf5079bac0
Ciar Daire <ciar.smythe@gmail.com>
2023-04-18 20:17:46 +1200

    Add pythondoc comments & tags

--- BowlingGame.py
+++ BowlingGame.py
@@ -2,12 +2,39 @@
 
 class BowlingGame:
     def __init__(self):
+        """
+        Initialises a list to store number of pins that will be knocked down in each roll.
+
+        Returns:
+            :rtype: An array.
+
+        """
         self.rolls=[]
 
     def roll(self,pins):
+        """
+        Tracks how many pins have been knocked down in each roll by adding it to the "rolls" list.
+
+        Args:
+            :param pins: Overall number of pins knocked down in each roll.
+            :type rollIndex: int
+
+        Returns:
+            :return: List with an integer representing number of pins knocked down.
+            :rtype: Array.
+
+        """
         self.rolls.append(pins)
 
     def score(self):
+        """
+        Calculates score by points recieved each frame.
+
+        Returns:
+            :return: Score of each frame.
+            :rtype: int
+
+        """
         result = 0
         rollIndex=0 
         for frameIndex in range(10):
@@ -25,18 +52,78 @@ class BowlingGame:
                 rollIndex +=1
         return result
 
-
     def isStrike(self, rollIndex):
+        """
+        Identifies a roll of 10 as a strike.
+
+        Args:
+            :param rollIndex: Current roll.
+            :type rollIndex: int
+
+        Returns:
+            :return: 10 points.
+            :rtype: int
+
+        """
         return self.rolls[rollIndex] == 10
+        print(isStrike.__doc__)
     
     def isSpare(self, rollIndex):
+        """
+        Identifies a frame (2 rolls) as a spare.
+
+        Args:
+            :param rollIndex: Current roll.
+            :type rollIndex: int
+
+        Returns:
+            :return: 10 points.
+            :rtype: int
+
+        """
         return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
     
     def strikeScore(self, rollIndex):
+        """
+        Calculates score/points of a strike by sum of the strike and the following 2 rolls (bonus points).
+
+        Args:
+            :param rollIndex: Current roll.
+            :type rollIndex: int
+
+        Returns:
+            :return: 10 + bonus points.
+            :rtype: int
+
+        """
         return 10+ self.rolls[rollIndex+1] + self.rolls[rollIndex+2]
         
     def spareScore(self, rollIndex):
+        """
+        Calculates score/points of a spare by sum of the spare and the following 1 roll (bonus points).
+
+        Args:
+            :param rollIndex: Current roll.
+            :type rollIndex: int
+
+        Returns:
+            :return: 10 + bonus point.
+            :rtype: int
+
+        """
         return  10+ self.rolls[rollIndex+2]
 
     def openFrameScore(self, rollIndex):
+        """
+        Calculates score/points of an open frame by sum of rolls within the frame, if less than 10 pins have been knocked down.
+
+        Args:
+            :param rollIndex: Current roll.
+            :type rollIndex: int
+
+        Returns:
+            :return: Points recieved in roll 1 and roll 2 of the frame.
+            :rtype: int
+
+        """
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
--- Test.py
+++ Test.py
@@ -6,18 +6,75 @@ import BowlingGame
 class TestBowlingGame(unittest.TestCase):
 
     def setUp(self):
+        """
+        Sets up connection to Bowling Game source code for unit tests to run.
+        """
         self.game = BowlingGame.BowlingGame()
 
     def testGutterGame(self):
+        """
+        Tests that player gets 0 points at the end of the game if they knock down 0 pins in all 10 frames / 20 rolls.
+
+        Args:
+            :param rolls: The overall range of rolls that have been made with the specified number of pins knocked in each roll.
+            :type: int
+
+            :param roll: The number of pins knocked down in each roll.
+            :type: int
+
+        Returns:
+            Expected to return score of 0 points.
+            :rtype: int
+
+            :raise: 'Assertion Error' if input does not return the expected score.
+            :raise: 'Unspported operant type(s)' if input type is invalid (e.g., not an int).
+
+        """
         for i in range(0, 20):
             self.game.roll(0)
         assert self.game.score() == 0
 
     def testAllOnes(self):
+        """
+        Tests that player gets 20 points at the end of the game if they knock down 1 pin per roll. Ensures that 1 pin is equal to 1 point.
+
+        Args:
+            :param rolls: The overall range of rolls that have been made with the specified number of pins knocked in each roll.
+            :type: int
+
+            :param roll: The number of pins knocked down in each roll.
+            :type: int
+
+        Returns:
+            :return: Expected to return score of 0 points.
+            :rtype: int
+
+            :raise: 'Assertion Error' if input does not return the expected score.
+            :raise: 'Unspported operant type(s)' if input type is invalid (e.g., not an int).
+
+        """
         self.rollMany(1, 20)
         assert self.game.score()==20
 
     def testOneSpare(self):
+        """
+        Tests that a series of rolls are correctly identified as a spare if the frame reaches 10, and calculates the score accordingly.
+
+        Args:
+            :param rolls: The overall range of rolls that have been made with the specified number of pins knocked in each roll.
+            :type: int
+
+            :param roll: The number of pins knocked down in each roll.
+            :type: int
+
+        Returns:
+            :return: Expected to return score of 16 points.
+            :rtype: int
+            
+            :raise: 'Assertion Error' if input does not return the expected score.
+            :raise: 'Unspported operant type(s)' if input type is invalid (e.g., not an int).
+
+        """
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
@@ -25,6 +82,24 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score()==16
 
     def testOneStrike(self):
+        """
+        Tests that a roll of 10 is identified as a strike, and calculates the score accordingly.
+
+        Args:
+            :param rolls: The overall range of rolls that have been made with the specified number of pins knocked in each roll.
+            :type: int
+
+            :param roll: The number of pins knocked down in each roll.
+            :type: int
+
+        Returns:
+            :return: Expected to return score of 24 points.
+            :rtype: int
+            
+            :raise: 'Assertion Error' if input does not return the expected score.
+            :raise: 'Unspported operant type(s)' if input type is invalid (e.g., not an int).
+
+        """
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
@@ -32,13 +107,59 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score()==24
 
     def testPerfectGame(self):
+        """
+        Tests that player gets 300 points at the end of the game if they get conseuctive strikes (a perfect game).
+
+        Args:
+            :param rolls: The overall range of rolls that have been made with the specified number of pins knocked in each roll.
+            :type: int
+
+            :param roll: The number of pins knocked down in each roll.
+            :type: int
+
+        Returns:
+            Expected to return score of 0 points.
+            :rtype: int
+
+            :raise: 'Assertion Error' if input does not return the expected score.
+            :raise: 'Unspported operant type(s)' if input type is invalid (e.g., not an int).
+
+        """
         self.rollMany(10,12)
         assert self.game.score()==300
 
     def testAllSpares(self):
+        """
+        Tests that player gets 150 points at the end of the game if they get conseuctive spares.
+
+        Args:
+            :param rolls: The overall range of rolls that have been made with the specified number of pins knocked in each roll.
+            :type: int
+
+            :param roll: The number of pins knocked down in each roll.
+            :type: int
+
+        Returns:
+            Expected to return score of 150 points.
+            :rtype: int
+
+            :raise: 'Assertion Error' if input does not return the expected score.
+            :raise: 'Unspported operant type(s)' if input type is invalid (e.g., not an int).
+
+        """
         self.rollMany(5,21)
         assert self.game.score()==150
         
     def rollMany(self, pins,rolls):
+        """
+        Function that knocks down the same number of pins for a specified number of rolls.
+
+        Args:
+            :param rolls: The overall range of rolls that have been made.
+            :type: int
+
+            :param pins: Overall number of pins knocked down in each roll.
+            :type rollIndex: int
+        """
         for i in range(rolls):
             self.game.roll(pins)
