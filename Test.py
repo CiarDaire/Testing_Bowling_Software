64d3bbe657ef425ad4b28f2d357bd8722ac4bb53
Ciar Daire <ciar.smythe@gmail.com>
2023-04-20 02:13:14 +1200

    assertEqual statements inputted into unit tests

--- Test.py
+++ Test.py
@@ -31,7 +31,7 @@ class TestBowlingGame(unittest.TestCase):
 
         """
         self.rollMany(0, 20)
-        assert self.game.score() == 0
+        self.assertEqual(self.game.score(), 0)
 
     def testAllOnes(self):
         """
@@ -53,7 +53,7 @@ class TestBowlingGame(unittest.TestCase):
 
         """
         self.rollMany(1, 20)
-        assert self.game.score()==20
+        self.assertEqual(self.game.score(), 20)
 
     def testOneSpare(self):
         """
@@ -78,7 +78,7 @@ class TestBowlingGame(unittest.TestCase):
         self.game.roll(5)
         self.game.roll(3)
         self.rollMany(0,17)
-        assert self.game.score()==16
+        self.assertEqual(self.game.score(), 16)
 
 
     def testOneStrike(self):
@@ -104,7 +104,7 @@ class TestBowlingGame(unittest.TestCase):
         self.game.roll(4)
         self.game.roll(3)
         self.rollMany(0,16)
-        assert self.game.score()==24
+        self.assertEqual(self.game.score(), 24)
 
     def testPerfectGame(self):
         """
@@ -126,7 +126,7 @@ class TestBowlingGame(unittest.TestCase):
 
         """
         self.rollMany(10,12)
-        assert self.game.score()==300
+        self.assertEqual(self.game.score(), 300)
 
     def testAllSpares(self):
         """
@@ -148,9 +148,9 @@ class TestBowlingGame(unittest.TestCase):
 
         """
         self.rollMany(5,21)
-        assert self.game.score()==150
+        self.assertEqual(self.game.score(), 150)
 
-    def rollMany(self, pins,rolls):
+    def rollMany(self, pins, rolls):
         """
         Function that knocks down the same number of pins for a specified number of rolls.
 
